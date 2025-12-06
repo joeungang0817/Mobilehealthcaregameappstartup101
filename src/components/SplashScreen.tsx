ff7bc800f9f0cec717ead67c81b4cc43e4ddbc8f
joeungang0817 <joeungang0817@gmail.com>
2025-12-07 01:22:46 +0900

    feat: char image

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -10,7 +10,7 @@ export function SplashScreen({ onComplete }: SplashScreenProps) {
   useEffect(() => {
     const timer = setTimeout(() => {
       onComplete();
-    }, 2500);
+    }, 20000);
 
     return () => clearTimeout(timer);
   }, [onComplete]);
